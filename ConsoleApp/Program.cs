fd77431760236816c9cde8e6d52639344322a3b6
lv-conner <lvmh123456@gmail.com>
2019-12-10 09:17:32 +0800

    修改转换

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,109 +6,182 @@ namespace ConsoleApp
 {
     class Program
     {
+        private static char[] Arr = null; 
+        private static int Ary
+        {
+            get
+            {
+                if(Arr == null)
+                {
+                    throw new InvalidOperationException();
+                }
+                else
+                {
+                    return Arr.Length;
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Convert33();
+            Init();
+            while(true)
+            {
+                Console.WriteLine("*****************************************************************************************************************************************");
+                Console.WriteLine("*************************************************************33进制转换*******************************************************************");
+                WriterConvertMap();
+                Console.WriteLine("***1.10进制转换为33进制***");
+                Console.WriteLine("***2.33进制转换为10进制***");
+                Console.WriteLine("***3.退出***");
+                Console.WriteLine("*****************************************************************************************************************************************");
+                var key = Console.ReadLine();
+                if(key == "quit")
+                {
+                    break;
+                }
+                if(key == "1")
+                {
+                    Convert33();
+                }
+                if(key == "2")
+                {
+                    ReverseConvert();
+                }
+            }
             Console.WriteLine("Hello World!");
+        }
+        private static void WriterConvertMap()
+        {
+            Console.WriteLine("*****************************************************************************************************************************************");
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                Console.Write(i + "-" + Arr[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine("*****************************************************************************************************************************************");
+        }
+        private static void Init()
+        {
+            var list = new List<char>()
+            {
+                '0',
+                '1',
+                '2',
+                '3',
+                '4',
+                '5',
+                '6',
+                '7',
+                '8',
+                '9',
+            };
+            char s = 'A';
+            list.Add(s);
+            for (int i = 1; i < 26; i++)
+            {
+                int value = (int)s;
+                value += i;
+                var newChar = (char)value;
+                list.Add(newChar);
+            }
+            list.Remove('I');
+            list.Remove('O');
+            list.Remove('U');
+            Arr = list.ToArray();
         }
         public static void ReverseConvert()
         {
-            var list = new List<char>()
+            while(true)
             {
-                '0',
-                '1',
-                '2',
-                '3',
-                '4',
-                '5',
-                '6',
-                '7',
-                '8',
-                '9',
-            };
-            char s = 'A';
-            list.Add(s);
-            for (int i = 1; i < 26; i++)
-            {
-                int value = (int)s;
-                value += i;
-                var newChar = (char)value;
-                list.Add(newChar);
+                Console.WriteLine("请输入33进制数，输入quit退出转换！");
+                var readStr = Console.ReadLine();
+                if (readStr == "quit")
+                {
+                    break;
+                }
+                var readValue = readStr.ToUpper();
+                bool validate = true;
+                foreach (var item in readValue)
+                {
+                    if (!Arr.Contains(item))
+                    {
+                        validate = false;
+                        break;
+                    }
+                }
+                if(!validate)
+                {
+                    Console.WriteLine("输入值不合法！");
+                }
+                else
+                {
+                    var arr = readValue.Reverse().ToArray();
+                    long rst = 0;
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        int value = 0;
+                        for (int j = 0; j < Arr.Length; j++)
+                        {
+                            if (Arr[j] == arr[i])
+                            {
+                                value = j;
+                                break;
+                            }
+                        }
+                        rst += value * (long)Math.Pow(Ary, i);
+                    }
+                    Console.WriteLine("输入值\t" + readStr);
+                    Console.WriteLine("转换值\t" + rst);
+                }
             }
-            list.Remove('I');
-            list.Remove('O');
-            var arr = list.ToArray();
-
-            var readStr = Console.ReadLine();
-            
         }
         public static void Convert33()
         {
-            var list = new List<char>()
-            {
-                '0',
-                '1',
-                '2',
-                '3',
-                '4',
-                '5',
-                '6',
-                '7',
-                '8',
-                '9',
-            };
-            char s = 'A';
-            list.Add(s);
-            for (int i = 1; i < 26; i++)
-            {
-                int value = (int)s;
-                value += i;
-                var newChar = (char)value;
-                list.Add(newChar);
-            }
-            list.Remove('I');
-            list.Remove('O');
-            var arr = list.ToArray();
-            Console.WriteLine(list.Aggregate("", (pre, next) => pre + ";" + next));
             string readString = null;
-            do
+            while(true)
             {
-                Console.WriteLine("enter int value");
+                Console.WriteLine("请输入10进制数，输入quit退出转换！");
                 readString = Console.ReadLine();
+                if(readString == "quit")
+                {
+                    break;
+                }
                 if (int.TryParse(readString, out var ConvertValue))
                 {
-                    if (ConvertValue < arr.Length)
+                    if (ConvertValue < Arr.Length)
                     {
-                        Console.WriteLine(arr[ConvertValue]);
+                        Console.WriteLine(Arr[ConvertValue]);
                     }
                     else
                     {
                         string rst = null;
                         do
                         {
-                            var remainder = ConvertValue % (arr.Length - 1);
+                            var remainder = ConvertValue % Arr.Length;
                             if (remainder == ConvertValue)
                             {
                                 break;
                             }
-                            rst += arr[remainder];
-
+                            rst += Arr[remainder];
                             ConvertValue -= remainder;
-                            var quotient = ConvertValue / (arr.Length - 1);
-                            if (quotient < (arr.Length - 1))
+                            var quotient = ConvertValue / Arr.Length;
+                            if (quotient < Arr.Length)
                             {
-                                rst += arr[quotient];
+                                rst += Arr[quotient];
                                 break;
                             }
                             ConvertValue = quotient;
                         }
                         while (true);
                         var reverse = rst.Reverse().Aggregate("", (pre, next) => pre + next);
-                        Console.WriteLine(reverse);
+                        Console.WriteLine("输入值\t" + readString);
+                        Console.WriteLine("转换值\t" + reverse);
                     }
                 }
+                else
+                {
+                    Console.WriteLine("输入值不合法！");
+                }
             }
-            while (readString != "quit");
         }
 
     }
